9098bcef1636c059e095f7bff2b6138948062ec0
Yolanda Robla <yroblamo@redhat.com>
2019-05-25 11:14:48 +0200

    Change permission execution to kustomize
    Change-Id: I2bf3509c58ffe88e0425e96df8655dfe7c2bdc69

--- utils/install_dependencies.sh
+++ utils/install_dependencies.sh
@@ -2,7 +2,7 @@
 
 URL=$(curl https://api.github.com/repos/kubernetes-sigs/kustomize/releases/latest |grep browser_download | grep linux | cut -d '"' -f 4)
 sudo -E curl -L $URL -o /usr/local/bin/kustomize
-sudo chmod u+x /usr/local/bin/kustomize
+sudo chmod a+x /usr/local/bin/kustomize
 
 wget -A "openshift-client-linux-4*\.tar\.gz" -r -np -nc -nd -l1 --no-check-certificate -e robots=off https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/ -P /tmp/
 sudo tar -xvf /tmp/openshift-client-linux-4*.tar.gz -C /usr/local/bin/ oc
