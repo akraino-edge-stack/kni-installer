2bac7137eab4ff4aef1260261d4f1bcc4f3d87b3
Yolanda Robla <yroblamo@redhat.com>
2019-07-04 18:03:12 +0200

    Add prepare_manifests endpoint
    Added new command to prepare the manifests for the site. It
    combines the original manifests declaration with the output of
    kustomize for the site, to generate a complete set of manifests,
    ready to be applied.
    
    Change-Id: I911bee254ee05322f2e683ce24391acdbfd81c72
    Signed-off-by: Yolanda Robla <yroblamo@redhat.com>

---
+++ cmd/prepare_manifests.go
@@ -0,0 +1,54 @@
+// Copyright © 2019 Red Hat <yroblamo@redhat.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"gerrit.akraino.org/kni/installer/pkg/site"
+	"github.com/spf13/cobra"
+)
+
+// prepareManifestsCmd represents the prepare_manifests command
+var prepareManifestsCmd = &cobra.Command{
+	Use:              "prepare_manifests",
+	Short:            "Command to prepare the manifests needed for a site",
+	Long:             ``,
+	TraverseChildren: true,
+	Run: func(cmd *cobra.Command, args []string) {
+		// retrieve config values and start fetching
+		siteRepo, _ := cmd.Flags().GetString("site")
+		buildPath, _ := cmd.Flags().GetString("build_path")
+		if len(buildPath) == 0 {
+			// will generate a temporary directory
+			buildPath = fmt.Sprintf("%s/.kni", os.Getenv("HOME"))
+		}
+
+		// define a site object and proceed with requirements fetch
+		s := site.New(siteRepo, buildPath)
+		s.DownloadSite()
+		s.PrepareManifests()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(prepareManifestsCmd)
+
+	prepareManifestsCmd.Flags().StringP("site", "", "", "Url/path for site repository. Can be in any go-getter compatible format")
+	prepareManifestsCmd.MarkFlagRequired("site")
+	prepareManifestsCmd.Flags().StringP("build_path", "", "", "Directory to use as build path. If that not exists, the installer will generate a default directory")
+
+}
