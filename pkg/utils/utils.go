c578870159756319195f80ac5fec0e11d6f811e4
Yolanda Robla <yroblamo@redhat.com>
2019-07-09 17:59:25 +0200

    Add new command to run workloads
    This command will apply kustomize on the extra
    workloads directories, and appy it with kubectl
    
    Signed-off-by: Yolanda Robla <yroblamo@redhat.com>
    
    Change-Id: I41639c99602cae2962533db807be951f293d2451

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"time"
 )
 
 // utility to validate pre-requisites for deploying
@@ -58,4 +59,41 @@ func ApplyKustomize(kustomizeBinary string, kustomizePath string) []byte {
 	}
 
 	return out
+}
+
+// utility to apply kubectl for a given output
+func ApplyKubectl(kubectlBinary string, kubectlContent []byte, kubeconfigPath string) {
+	modified := false
+	var out []byte
+	var err string
+	for i := 1; i <= 10; i++ {
+		cmd := exec.Command(kubectlBinary, "apply", "-f", "-")
+
+		cmd.Env = os.Environ()
+		cmd.Env = append(cmd.Env, fmt.Sprintf("KUBECONFIG_PATH=%s", kubeconfigPath))
+
+		// add output to stdin
+		stdin, err := cmd.StdinPipe()
+		stdin.Write(kubectlContent)
+		stdin.Close()
+
+		out, err = cmd.Output()
+
+		// show output for user to see progress
+		log.Println(string(out))
+
+		if err == nil {
+			// it is ok, stop the loop
+			modified = true
+			break
+		} else {
+			// sleep and retry
+			time.Sleep(60 * time.Second)
+		}
+	}
+
+	// if not modified, show error
+	if !modified {
+		log.Println(fmt.Sprintf("Error applying workloads: %s", err))
+	}
 }
