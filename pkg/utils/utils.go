e154b8833c46501dc38d66657905642daa388f62
Yolanda Robla <yroblamo@redhat.com>
2019-07-16 12:28:24 +0200

    Create wrapper to execute commands
    In order to have a standard way of executing shell
    commands, and show the proper output/error, i have
    created a wrapper that is called on each system call,
    and controls the behaviour of it.
    
    Signed-off-by: Yolanda Robla <yroblamo@redhat.com>
    
    Change-Id: I0a40a126677a5f7ec878e34e657399d696dd5ee9

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -22,12 +23,8 @@ func ValidateRequirements(buildPath string, siteName string) {
 	if _, err := os.Stat(fmt.Sprintf("%s/id_rsa.pub", buildPath)); os.IsNotExist(err) {
 		log.Println(fmt.Sprintf("No SSH public key (id_rsa.pub) found in %s. Generating keypair.", buildPath))
 
-		cmd := exec.Command("/bin/bash", "-c", fmt.Sprintf("ssh-keygen -b 2048 -q -N '' -f %s/id_rsa -C user@example.com", buildPath))
-		err = cmd.Run()
-		if err != nil {
-			log.Fatal(fmt.Sprintf("Error generating ssh keypair: %s", err))
-			os.Exit(1)
-		}
+		var envVars []string
+		ExecuteCommand("", envVars, true, true, "/bin/bash", "-c", fmt.Sprintf("ssh-keygen -b 2048 -q -N '' -f %s/id_rsa -C user@example.com", buildPath))
 	}
 
 	// check if requirements folder exist
@@ -49,15 +46,8 @@ func ApplyKustomize(kustomizeBinary string, kustomizePath string) []byte {
 	}
 	exPath := filepath.Dir(ex)
 
-	cmd := exec.Command(kustomizeBinary, "build", "--enable_alpha_plugins", "--reorder", "none", kustomizePath)
-	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env, fmt.Sprintf("XDG_CONFIG_HOME=%s/plugins", exPath))
-	out, err := cmd.Output()
-
-	if err != nil {
-		log.Fatal(fmt.Sprintf("Error kustomizing manifests for %s: %s", kustomizePath, err))
-		os.Exit(1)
-	}
+	envVars := []string{fmt.Sprintf("XDG_CONFIG_HOME=%s/plugins", exPath)}
+	out, _ := ExecuteCommand("", envVars, true, false, kustomizeBinary, "build", "--enable_alpha_plugins", "--reorder", "none", kustomizePath)
 
 	return out
 }
@@ -80,15 +70,10 @@ func ApplyKubectl(kubectlBinary string, kubectlContent []byte, kubeconfigPath st
 		os.Exit(1)
 	}
 
+    envVars := []string{fmt.Sprintf("KUBECONFIG_PATH=%s", kubeconfigPath)}
 	for i := 1; i <= 10; i++ {
-		cmd := exec.Command(kubectlBinary, "apply", "-f", tmpFile.Name())
-		cmd.Env = os.Environ()
-		cmd.Env = append(cmd.Env, fmt.Sprintf("KUBECONFIG_PATH=%s", kubeconfigPath))
-
-		out, err = cmd.Output()
-
-		// show output for user to see progress
-		log.Println(string(out))
+	for i := 1; i <= 10; i++ {
+		_, err := ExecuteCommand("", envVars, false, true, kubectlBinary, "apply", "-f", tmpFile.Name())
 
 		if err == nil {
 			// it is ok, stop the loop
@@ -99,4 +84,38 @@ func ApplyKubectl(kubectlBinary string, kubectlContent []byte, kubeconfigPath st
 			time.Sleep(60 * time.Second)
 		}
 	}
+}
+
+// utility to execute a command and show the stdout and stderr output
+func ExecuteCommand(directory string, envVars []string, failFatal bool, showOutput bool, name string, arg ...string) ([]byte, []byte) {
+	cmd := exec.Command(name, arg...)
+
+	// set additional modifiers
+	if directory != "" {
+		cmd.Dir = directory
+	}
+	cmd.Env = os.Environ()
+	for _, envVar := range envVars {
+		cmd.Env = append(cmd.Env, envVar)
+
+	}
+	var outb, errb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb
+	err := cmd.Run()
+
+	// show output of command
+	if showOutput {
+		log.Println(outb.String())
+	}
+
+	if err != nil {
+		if failFatal {
+			log.Fatal("Error applying command: %s - %s", err, errb.String())
+			os.Exit(1)
+		} else {
+			log.Println("Error applying command: %s - %s", err, errb.String())
+		}
+	}
+	return outb.Bytes(), errb.Bytes()
 }
