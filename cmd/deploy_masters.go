43e7f752ed025848e77127a74b7f48f1fff4dfde
Andrew Bays <abays@redhat.com>
2019-10-08 06:57:59 -0400

    Automated masters and workers deploy
    Change-Id: I9ed643e4fee34fef6c73bbf0a5266c9cfba401db

---
+++ cmd/deploy_masters.go
@@ -0,0 +1,60 @@
+// Copyright © 2019 Red Hat <abays@redhat.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gerrit.akraino.org/kni/installer/pkg/site"
+	"github.com/spf13/cobra"
+)
+
+// deployMastersCmd represents the automate_masters_deployment command
+var deployMastersCmd = &cobra.Command{
+	Use:              "deploy_masters siteName [--build_path=<local_build_path>]",
+	Short:            "Command to automate the deployment of the master nodes of a previously-prepared site",
+	Long:             ``,
+	TraverseChildren: true,
+	Run: func(cmd *cobra.Command, args []string) {
+		// retrieve config values and start fetching
+		var siteName string
+		if len(args) == 0 {
+			log.Fatal("Please specify site name as first argument")
+			os.Exit(1)
+		} else {
+			siteName = args[0]
+		}
+
+		buildPath, _ := cmd.Flags().GetString("build_path")
+		if len(buildPath) == 0 {
+			// will generate a temporary directory
+			buildPath = fmt.Sprintf("%s/.kni", os.Getenv("HOME"))
+		}
+
+		// This command is used after fetch_requirements and prepare_manifests,
+		// so the site directory should be available on disk already (if not,
+		// s.AutomateMastersDeployment will error-out appropriately)
+		s := site.NewWithName(siteName, buildPath)
+		s.AutomateMastersDeployment()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deployMastersCmd)
+
+	deployMastersCmd.Flags().StringP("build_path", "", "", "Directory to use as build path. If that doesn't exist, the installer will generate a default directory")
+}
--- pkg/automation/automation.go
+++ pkg/automation/automation.go
@@ -5,6 +5,16 @@ import (
 	"fmt"
 )
 
+// ProfileType, SiteBuildPath and SiteName are always needed (and are always
+// checked by the automation package's New function).  Anything else added here
+// is the responsibility of the automation interface implementation to validate
+type AutomatedDeploymentParams struct {
+	ProfileType   string
+	SiteBuildPath string
+	SiteName      string
+	SiteRepo      string
+}
+
 type AutomatedDeploymentInterface interface {
 	PrepareBastion() error // Prepare host for automation
 	DeployMasters() error  // Deploy cluster masters
@@ -15,39 +25,38 @@ var (
 	// If we find that different profile types (libvirt, aws, etc) that we add
 	// in the future require different constructor parameter count/types, then
 	// we can redo the approach here
-	automatedDeploymentConstructors map[string]func(string, string, string) (AutomatedDeploymentInterface, error)
+	automatedDeploymentConstructors map[string]func(AutomatedDeploymentParams) (AutomatedDeploymentInterface, error)
 )
 
 func init() {
 	// Add new automation profile types here
-	automatedDeploymentConstructors = map[string]func(string, string, string) (AutomatedDeploymentInterface, error){}
+	automatedDeploymentConstructors = map[string]func(AutomatedDeploymentParams) (AutomatedDeploymentInterface, error){}
 	automatedDeploymentConstructors["baremetal"] = newBaremetal
 }
 
 // Generates a new automation deployment instance
-func New(profileType string, siteBuildPath string, siteName string, siteRepo string) (AutomatedDeploymentInterface, error) {
-	if siteBuildPath == "" {
+func New(params AutomatedDeploymentParams) (AutomatedDeploymentInterface, error) {
+	// SiteBuildPath is always needed
+	if params.SiteBuildPath == "" {
 		return nil, errors.New("AutomatedDeployment: New: site build path not provided")
 	}
 
-	if siteName == "" {
+	// SiteName is always needed
+	if params.SiteName == "" {
 		return nil, errors.New("AutomatedDeployment: New: site name not provided")
 	}
 
-	if siteRepo == "" {
-		return nil, errors.New("AutomatedDeployment: New: site repo not provided")
-	}
-
-	constructor := automatedDeploymentConstructors[profileType]
+	// ProfileType is always needed
+	constructor := automatedDeploymentConstructors[params.ProfileType]
 
 	// If no constructor available, then automation is not available for this profile type
 	if constructor == nil {
-		return nil, fmt.Errorf("AutomatedDeployment: New: automation not supported for profile type '%s", profileType)
+		return nil, fmt.Errorf("AutomatedDeployment: New: automation not supported for profile type '%s", params.ProfileType)
 	}
 
 	// Constructors should return nil as the AutomatedDeploymentInterface if automation is
 	// not supported for the particular site
-	automatedDeployment, err := constructor(siteBuildPath, siteName, siteRepo)
+	automatedDeployment, err := constructor(params)
 
 	if err != nil {
 		return nil, err
